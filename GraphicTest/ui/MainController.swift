710e42ab076e6a25e0c85cefa6b5acefadbd65fd
vb.antipov <vb.antipov@tensor.ru>
2018-06-25 19:10:29 +0700

    запускаю в бэкграунде

--- GraphicTest/ui/MainController.swift
+++ GraphicTest/ui/MainController.swift
@@ -9,48 +9,55 @@
 import UIKit
 
 class MainController: UIViewController {
-
-  @IBOutlet var graphView: GraphView!
-  var service: GraphicService!
-  struct Constant{
-    static let dx: CGFloat = 1.0
-  }
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    service = GraphicService()
-  }
-  
-  @IBAction func loadData(_ sender: Any) {
-    load()
-  }
-  
-  func load() {
-    do {
-      let graphic = try service.load(maxCount: 5)
-      drawGraph(graphic)
-    } catch {
-      let alert = ErrorHandler.alert(error)
-      present(alert, animated: true)
-    }
-  }
-  
-  func drawGraph(_ graphic: Graphic) {
-    let inputPoints = graphic.points
-    graphView.series.append((inputPoints, UIColor.red))
-    
-    let akima = AkimaInterpolator(points: inputPoints)
-    
-    var splinePoints = [CGPoint]()
-    var x = inputPoints.first!.x
-    while x<=inputPoints.last!.x {
-      
-      splinePoints.append(CGPoint(x: x, y: akima.interpolate(value: x)))
-      x += Constant.dx
-    }
-    
-    graphView.series.append((splinePoints, UIColor.blue))
-    graphView.setNeedsDisplay()
-  }
-
+	
+	@IBOutlet var graphView: GraphView!
+	var service: GraphicService!
+	struct Constant {
+		static let dx: CGFloat = 1.0
+	}
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		service = GraphicService()
+	}
+	
+	@IBAction func loadData(_: Any) {
+		load()
+	}
+	
+	func load() {
+		DispatchQueue.global(qos: .default).async { [weak self] in
+			guard let strong = self else {
+				return
+			}
+			do {
+				let graphic = try strong.service.load(maxCount: 5)
+				DispatchQueue.main.async {
+					strong.drawGraph(graphic)
+				}
+			} catch {
+				let alert = ErrorHandler.alert(error)
+				strong.present(alert, animated: true)
+			}
+		}
+	}
+	
+	func drawGraph(_ graphic: Graphic) {
+		let inputPoints = graphic.points
+		graphView.series.append((inputPoints, UIColor.red))
+		
+		let akima = AkimaInterpolator(points: inputPoints)
+		
+		var splinePoints = [CGPoint]()
+		var x = inputPoints.first!.x
+		while x <= inputPoints.last!.x {
+			
+			splinePoints.append(CGPoint(x: x, y: akima.interpolate(value: x)))
+			x += Constant.dx
+		}
+		
+		graphView.series.append((splinePoints, UIColor.green))
+		graphView.setNeedsDisplay()
+	}
+	
 }
